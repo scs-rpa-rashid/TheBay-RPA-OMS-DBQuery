38c88764842d414bc9e82d0a3b05451fcada3805
varun-kumarv <153303531+varun-kumarv@users.noreply.github.com>
2024-05-30 21:32:25 +0530

    Delete DeepNet_Performer directory

--- DeepNet_Performer/src/main/java/utils/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class BusinessException extends Exception {
-
-    public BusinessException(String message) {
-
-        super(message);
-    }
-}
--- DeepNet_Performer/src/main/java/utils/SystemException.java
+++
@@ -1,8 +0,0 @@
-package utils;
-
-public class SystemException extends Exception{
-
-    public SystemException(String message){
-        super(message);
-    }
-}
